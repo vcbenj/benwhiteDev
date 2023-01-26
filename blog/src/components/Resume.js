e46128e9c4b58620019e80089791fc5fe862e0e8
vcbenj <whi15026@byui.edu>
2023-01-25 22:41:22 -0500

    Base resume

---
+++ blog/src/components/Resume.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./../App.css";
+
+const Resume = () => {
+    return (
+        <div> 
+        <h1> Resume</h1>
+        <p> 505-615-7585 | vcbenj@gmail.com | https://www.linkedin.com/in/benwhite-</p>
+        <h3> Work History</h3>
+        <h3> Software Engineer</h3>
+        </div>
+    )
+
+}
+
+export default Resume;
