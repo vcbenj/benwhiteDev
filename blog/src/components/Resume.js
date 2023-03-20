03b7d5a74f5fde59e29d7e6794b7f25fd3d99626
vcbenj <vcbenj@gmail.com>
2023-03-20 10:50:27 -0400

    add Resume

--- blog/src/components/Resume.js
+++ blog/src/components/Resume.js
@@ -1,14 +1,14 @@
 import React from "react";
+import { Document,Page } from 'react-pdf/dist/esm/entry.webpack';
+import pdfFile from './Resume.pdf'
 
 import "./../App.css";
+// import SEResume from "./SEResume.html";
 
 const Resume = () => {
     return (
-        <div> 
-        <h1> Resume</h1>
-        <p> 505-615-7585 | vcbenj@gmail.com | https://www.linkedin.com/in/benwhite-</p>
-        <h3> Work History</h3>
-        <h3> Software Engineer</h3>
+        <div class="resume"> 
+         <iframe src={pdfFile} frameborder="0" height="1000em" width="100%"></iframe>
         </div>
     )
 
